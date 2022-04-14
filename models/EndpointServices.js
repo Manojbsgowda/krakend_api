15a2ec04586306ce71ffa40ac1f4168c35c33efa
Manojbsgowda <manojgowdas996@gmail.com>
2022-04-14 15:22:55 +0530

    changes made to the backend

--- models/EndpointServices.js
+++ models/EndpointServices.js
@@ -4,16 +4,31 @@ const endpointSchema = new mongoose.Schema(
   {
     endpoint: { type: String },
     method: { type: String },
-    input_query_strings: { type: Array },
+    extra_config: {
+      qos_ratelimit_router: {
+        max_rate: { type: String },
+        client_max_rate: { type: String },
+        strategy: { type: String, default: "ip" },
+      },
+    },
+    input_query_strings: { type: [String] },
     input_headers: { type: Array },
     concurrent_calls: { type: String },
-    max_rate: { type: String },
-    client_max_rate: { type: String },
-
-    url_pattern: { type: String },
-    method: { type: String },
-    max_rate: { type: String },
-    capacity: { type: String },
+    backend: {
+      type: [
+        {
+          url_pattern: { type: String },
+          method: { type: String },
+          extra_config: {
+            qos_ratelimit_proxy: {
+              max_rate: { type: String },
+              capacity: { type: String },
+            },
+          },
+          host: { type: [String] },
+        },
+      ],
+    },
     servicesId: { type: String, ref: "Services" },
   },
   {
--- routes/endpointServices.js
+++ routes/endpointServices.js
@@ -1,24 +1,11 @@
 const express = require("express");
 const EndpointServices = require("../models/EndpointServices");
+const Services = require("../models/Services");
 const router = express.Router();
 
 router.post("/create", async (req, res) => {
   try {
-    const newEndpointService = new EndpointServices({
-      endpoint: req.body.krakend_endpoint,
-      method: req.body.http_method,
-      input_query_strings: req.body.parameters,
-      input_headers: req.body.headers_passing_to_backend,
-      concurrent_calls: req.body.concurrent_calls,
-      max_rate: req.body.rate_limit,
-      client_max_rate: req.body.user_quota,
-
-      url_pattern: req.body.backend_endpoint,
-      method: req.body.endpoint_http_method,
-      max_rate: req.body.endpoint_rate_limit,
-      capacity: req.body.Capacity_burstsize,
-      servicesId: req.body.servicesId,
-    });
+    const newEndpointService = new EndpointServices(req.body);
     const saved = await newEndpointService.save();
     res.status(201).json({ saved, message: "endpoint data saved" });
   } catch (error) {
@@ -26,37 +13,27 @@ router.post("/create", async (req, res) => {
   }
 });
 
-router.get("/", async (req, res) => {
+router.get("/:id", async (req, res) => {
+  const serv_id = req.params.id;
   try {
-    let endpointList = [];
+    const serviceConfig = await Services.findOne({
+      _id: serv_id,
+    }).select(["-_id", "-createdAt", "-updatedAt", "-__v"]);
+
     const data = await EndpointServices.find({
-      servicesId: "6254029f2d41a43a4df4182f",
-    });
-    // const originalFormat = {
-    //   endpoint:,
-    //   method:,
-    //   extra_config:{
-    //     qos/ratelimit/router:{
-    //       max_rate:,
-    //       client_max_rate:,
-    //       strategy: ,
-    //     }
-    //   },
+      servicesId: serv_id,
+    }).select([
+      "-_id",
+      "-backend._id",
+      "-servicesId",
+      "-createdAt",
+      "-updatedAt",
+      "-__v",
+    ]);
 
-    //   backend:[
-    //     {
-    //       url_pattern:,
-    //       method:,
-    //       extra_config:{
-    //         qos/ratelimit/proxy:,
-    //         max_rate:,
-    //         capacity:,
-    //       }
-    //     },
-    //   ]
+    const d = {};
 
-    // }
-    res.status(200).json({ data });
+    res.status(200).json({ ...serviceConfig._doc, endpoints: data });
   } catch (error) {
     res.status(500).json({ error, message: "not getting any value" });
   }
